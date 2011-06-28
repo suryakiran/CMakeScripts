71ebb03b1c8675f229045ea648ec66b49463fecd
Surya Kiran Gullapalli <SuryaKiran.Gullapalli@gmail.com>
2011-06-28 17:53:22 +0530

    Some variables. This file is going to be changed

--- Dirs.cmake
+++ Dirs.cmake
@@ -2,3 +2,9 @@ Get_Filename_Component (CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/CMake ABSOLUTE CAC
 Get_Filename_Component (CMAKE_PERL_DIR ${CMAKE_MODULE_PATH}/perl ABSOLUTE CACHE)
 Get_Filename_Component (CMAKE_PYTHON_DIR ${CMAKE_MODULE_PATH}/python ABSOLUTE CACHE)
 Get_Filename_Component (CMAKE_CONFIG_DIR ${CMAKE_SOURCE_DIR}/Config ABSOLUTE CACHE)
+
+Get_Filename_Component (CMAKE_DLL_H_IN_FILE ${CMAKE_MODULE_PATH}/Configure/Dll.h.in ABSOLUTE CACHE)
+
+If (WIN32)
+  Add_Definitions ("-DWINDOWS")
+EndIf (WIN32)
