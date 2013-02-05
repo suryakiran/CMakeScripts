d158c423fd007f370c73aa1d02e4bcebb51f23cd
Surya Kiran Gullapalli <SuryaKiran.Gullapalli@gmail.com>
2013-02-05 12:58:34 +0530

    Enabling Cxx-11

--- C++-11.cmake
+++ C++-11.cmake
@@ -1,4 +1,5 @@
 Include (CheckCXXSourceCompiles)
+Include (CMakePushCheckState)
 
 Function (CHECK_CXX11_FEATURE)
   Parse_Arguments (
@@ -11,45 +12,28 @@ Function (CHECK_CXX11_FEATURE)
   EndIf (CXX11_FEATURE_FILE)
 
   If (NOT ${CXX11_FEATURE_VAR})
-    Message (STATUS "Checking for C++-11 feature: " ${CXX11_FEATURE_NAME})
-
     If (UNIX)
-      Try_Compile (
-        result
-        ${CMAKE_BINARY_DIR}/C++11-Tests
-        ${CMAKE_MODULE_PATH}/Src/${CXX11_FEATURE_FILE}.cxx
-        CMAKE_FLAGS -DCOMPILE_DEFINITIONS:STRING=${COMPILER_FLAG_CXX11}
-        OUTPUT_VARIABLE CXX11_FEATURE_OUTPUT
-        )
+      Cmake_Push_Check_State()
+      Set(CMAKE_REQUIRED_FLAGS "${COMPILER_FLAG_CXX11}")
+      Check_Cxx_Source_Compiles("${FILE_AS_STRING}" ${CXX11_FEATURE_VAR})
+      Cmake_Pop_Check_State()
     Else (UNIX)
-      Try_Compile (
-        result
-        ${CMAKE_BINARY_DIR}/C++11-Tests
-        ${CMAKE_MODULE_PATH}/Src/${CXX11_FEATURE_FILE}.cxx
-        OUTPUT_VARIABLE CXX11_FEATURE_OUTPUT
-        )
+      Check_Cxx_Source_Compiles("${FILE_AS_STRING}" ${CXX11_FEATURE_VAR})
     EndIf (UNIX)
-
-    If (result)
-      Set (${CXX11_FEATURE_VAR} 1 CACHE INTERNAL "Support for C++ feature: ${CXX11_FEATURE_NAME}")
-      Message (STATUS "Checking for C++-11 feature: ${CXX11_FEATURE_NAME} -- found")
-    Else (result)
-      Set (${CXX11_FEATURE_VAR} 0 CACHE INTERNAL "Support for C++ feature: ${CXX11_FEATURE_NAME}")
-      Message (STATUS "Checking for C++-11 feature: ${CXX11_FEATURE_NAME} -- not found")
-    EndIf (result)
-
   EndIf (NOT ${CXX11_FEATURE_VAR})
 EndFunction (CHECK_CXX11_FEATURE)
 
-Check_Cxx11_Feature(NAME "auto" FILE TestAuto VAR CXX11_FEATURE_AUTO)
-Check_Cxx11_Feature(NAME "lambda" FILE TestLambda VAR CXX11_FEATURE_LAMBDA)
-Check_Cxx11_Feature(NAME "nullptr" FILE TestNullPtr VAR CXX11_FEATURE_NULLPTR)
-Check_Cxx11_Feature(NAME "shared_ptr" FILE TestSharedPtr VAR CXX11_FEATURE_SHARED_PTR)
-Check_Cxx11_Feature(NAME "unique_ptr" FILE TestUniquePtr VAR CXX11_FEATURE_UNIQUE_PTR)
-Check_Cxx11_Feature(NAME "Fixed size array" FILE TestArray VAR CXX11_FEATURE_ARRAY)
-
-Check_Cxx11_Feature(
-  NAME "Non-static data member initializers"  
-  FILE TestNonStaticDataInit
-  VAR CXX11_FEATURE_NON_STATIC_DATA_INIT
-  )
+If (CMAKE_COMPILER_IS_GNUCXX AND CXX11_SUPPORT_FLAG)
+  Check_Cxx11_Feature(NAME "auto" FILE TestAuto VAR CXX11_FEATURE_AUTO)
+  Check_Cxx11_Feature(NAME "lambda" FILE TestLambda VAR CXX11_FEATURE_LAMBDA)
+  Check_Cxx11_Feature(NAME "nullptr" FILE TestNullPtr VAR CXX11_FEATURE_NULLPTR)
+  Check_Cxx11_Feature(NAME "shared_ptr" FILE TestSharedPtr VAR CXX11_FEATURE_SHARED_PTR)
+  Check_Cxx11_Feature(NAME "unique_ptr" FILE TestUniquePtr VAR CXX11_FEATURE_UNIQUE_PTR)
+  Check_Cxx11_Feature(NAME "Fixed size array" FILE TestArray VAR CXX11_FEATURE_ARRAY)
+  
+  Check_Cxx11_Feature(
+    NAME "Non-static data member initializers"  
+    FILE TestNonStaticDataInit
+    VAR CXX11_FEATURE_NON_STATIC_DATA_INIT
+    )
+EndIf (CMAKE_COMPILER_IS_GNUCXX AND CXX11_SUPPORT_FLAG)
